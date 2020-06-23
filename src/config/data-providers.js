b1d0a41a28d27d2e1a12c1b91545a90e99c75787
David Coterillo Gonzalez <coterillogd@unican.es>
2020-06-23 15:48:38 +0200

    config mapper

--- src/config/data-providers.js
+++ src/config/data-providers.js
@@ -1,9 +1,4 @@
-export const MERCURIO_API = {
-  public: process.env.VUE_APP_API_PUBLIC_MERCURIO,
-  private: process.env.VUE_APP_API_PRIVATE_MERCURIO
-};
-
-export const SENSORS_API = {
-  public: process.env.VUE_APP_API_PUBLIC_SENSORS,
-  private: process.env.VUE_APP_API_PRIVATE_SENSORS
+export const RIOS_API = {
+  public: process.env.VUE_APP_API_PUBLIC_RIOS,
+  private: process.env.VUE_APP_API_PRIVATE_RIOS
 };
--- src/mixins/data-helper.js
+++ src/mixins/data-helper.js
@@ -1,133 +1,35 @@
-import {
-  parseGeoJSON,
-  colorLayers,
-  mapIDGenerator,
-  licenseProductType,
-  licenseBuoyType,
-  licenseStationType
-} from "@/utils/data-operator-utils.js";
-import { mapGetters } from "vuex";
-import * as types from "@/store/types";
 import * as config from "@/config/data-config";
+import {
+  getBiologicalColor,
+  getForestColor,
+  getStateColor
+} from "@/utils/data-operator-utils.js";
 export const dataHelperMixin = {
-  computed: {
-    ...mapGetters({
-      getSourceName: [types.G_GET_SOURCE_NAMESHORT],
-      getSourceType: [types.G_GET_SOURCE_TYPE],
-      getBuoyTypeName: [types.G_GET_BUOYTYPE_NAME]
-    })
-  },
   methods: {
-    formatProducts(data) {
-      return data
-        .filter(data => data.active)
-        .map(data => {
-          return {
-            id: data.id,
-            mapID: mapIDGenerator(data.id, config.PRODUCTS.id),
-            selected: false,
-            type: config.PRODUCTS.type,
-            alias: data.alias,
-            sourceName: data.source.alias,
-            source: data.source,
-            sourceOrigin: this.getSourceType(data.source.id),
-            extent: parseGeoJSON(data.extent),
-            variables: data.variables,
-            productType: data.type,
-            license: data.license,
-            licenseOpen: licenseProductType(data.license),
-            color: colorLayers(),
-            temporalResolution: data.timeResolution
-              ? parseFloat(data.timeResolution)
-              : "",
-            spatialResolution: data.spatialResLonKm,
-            catalogUrl: `${data.urlBase}${data.catalog}`
-          };
-        });
-    },
-    formatSources(data) {
+    formatSamples(data) {
       return data.map(data => {
         return {
+          bioQuality: data.bioQualityName,
+          ecologicalState: data.ecologicalStateName,
+          forestState: data.riverBankQrisiName,
+          alias: data.riverSectionAlias,
+          municipality: data.riverSectionMunicipality,
+          river: data.riverName,
+          riverType: data.riverType,
+          catchment: data.riverCatchment,
           id: data.id,
-          nameLong: data.nameLong ? data.nameLong : "",
-          nameShort: data.nameShort ? data.nameShort : "",
-          urlEntity: data.urlEntity ? data.urlEntity : "",
-          sourceType: data.sourceType ? data.sourceType : ""
-        };
-      });
-    },
-    formatBuoys(data) {
-      return data.map(data => {
-        return {
-          id: data.id,
-          mapID: mapIDGenerator(data.id, config.BUOYS.id),
-          selected: false,
-          name: data.name,
-          type: config.BUOYS.type,
-          typeSensor: config.BUOYS.typeSensor,
-          subTypeSensors: this.getBuoyTypeName(data.buoyTypeId),
-          distance: data.distance ? data.distance : 0,
-          latitude: data.latitude ? data.latitude : 0,
-          longitude: data.longitude ? data.longitude : 0,
-          license:
-            this.getBuoyTypeName(data.buoyTypeId) == "Restringida"
-              ? "Restricted Access"
-              : "Open Access",
-          licenseOpen: licenseBuoyType(this.getBuoyTypeName(data.buoyTypeId)),
-          variables: data.variable,
-          color: config.BUOYS.color
-        };
-      });
-    },
-    formatStations(data) {
-      return data
-        .filter(data => data.active)
-        .map(data => {
-          return {
-            id: data.code,
-            mapID: mapIDGenerator(data.code, config.STATIONS.id),
-            selected: false,
-            name: data.name,
-            type: config.STATIONS.type,
-            typeSensor: config.STATIONS.typeSensor,
-            subTypeSensors: data.stationType.name,
-            distance: data.distance ? data.distance : 0,
-            height: data.height ? data.height : 0,
-            latitude: data.latitude ? data.latitude : 0,
-            longitude: data.longitude ? data.longitude : 0,
-            placeName: data.placeName ? data.placeName : "",
-            project: data.project ? data.project.name : "",
-            color: config.STATIONS.color,
-            variables: [],
-            license: "Open Access",
-            licenseOpen: licenseStationType("Open Access")
-          };
-        });
-    },
-    formatProductMoments(data) {
-      return data
-        .filter(
-          d =>
-            config.TIMELINE_MOMENTS.filter(moment => moment.id == d.id).length >
-            0
-        )
-        .map(data => {
-          let configMoment = config.TIMELINE_MOMENTS.find(d => d.id == data.id);
-          return {
-            id: data.id,
-            label: configMoment.label,
-            icon: configMoment.icon,
-            name: data.moment,
-            color: configMoment.color
-          };
-        });
-    },
-    formatProductVariableTypes(data, additionalInfo) {
-      return data.map(data => {
-        return {
-          id: data.id,
-          name: data.name,
-          momentId: additionalInfo.momentId
+          idBioQuality: data.idBioQuality,
+          bioQualityColor: getBiologicalColor(data.idBioQuality),
+          idForestState: data.idRiverBankQrisi,
+          forestStateColor: getForestColor(data.idRiverBankQrisi),
+          idEcologicalState: data.idEcologicalState,
+          ecologicalStateColor: getStateColor(data.idEcologicalState),
+          campaign: data.campaign,
+          year: data.year,
+          type: config.SAMPLES.type,
+          iconBiological: config.SAMPLES.iconBiological,
+          iconForest: config.SAMPLES.iconForest,
+          iconState: config.SAMPLES.iconState
         };
       });
     }
