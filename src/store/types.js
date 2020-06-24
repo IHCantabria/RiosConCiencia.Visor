4e73c39d71a29e4f6c8e22602a0a038aa0845d06
David Coterillo Gonzalez <coterillogd@unican.es>
2020-06-24 16:27:49 +0200

    data logic and store

--- src/store/types.js
+++ src/store/types.js
@@ -1,7 +1,11 @@
 //Mutations
 export const M_SET_SAMPLES = "samples/M_SET_SAMPLES";
 export const M_SET_SAMPLES_FULL = "samples/M_SET_SAMPLES_FULL";
+export const M_SET_YEAR_OPTIONS = "samples/M_SET_YEAR_OPTIONS";
 
 //Actions
 export const A_FETCH_SAMPLES = "samples/A_FETCH_SAMPLES";
-export const A_FETCH_SAMPLES_FULL = "datahub/A_FETCH_SAMPLES_FULL";
+export const A_FETCH_SAMPLES_FULL = "samples/A_FETCH_SAMPLES_FULL";
+
+//Getters
+export const G_GET_SAMPLES_FILTERED = "samples/G_GET_SAMPLES_FILTERED";
