f5b519cb65552715a991a04e15e1436af840465c
David Coterillo Gonzalez <coterillogd@unican.es>
2020-06-25 22:19:44 +0200

    panels logic

--- src/components/layout/AppPanels.vue
+++ src/components/layout/AppPanels.vue
@@ -3,9 +3,18 @@
     <panel-about class="panel-about"></panel-about>
     <panel-filters></panel-filters>
     <panel-login></panel-login>
+    <vs-button
+      v-if="userLogged"
+      color="primary"
+      type="filled"
+      @click="launchDownload"
+      >Descargar Datos</vs-button
+    >
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+import * as types from "@/store/types";
 import AppAbout from "@/components/layout/AppAbout.vue";
 import PanelFilters from "@/components/PanelFilters.vue";
 import PanelLogin from "@/components/PanelLogin.vue";
@@ -14,6 +23,16 @@ export default {
     "panel-about": AppAbout,
     "panel-login": PanelLogin,
     "panel-filters": PanelFilters
+  },
+  computed: {
+    ...mapGetters({
+      userLogged: [types.G_GET_USER_LOGGED]
+    })
+  },
+  methods: {
+    launchDownload() {
+      this.$root.$emit("callDownload");
+    }
   }
 };
 </script>
