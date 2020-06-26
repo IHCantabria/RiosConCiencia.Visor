dacde82064be62aaa4618b41cc28b781f0446058
David Coterillo Gonzalez <coterillogd@unican.es>
2020-06-26 11:13:34 +0200

    dev tools change

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   chainWebpack: config => {
     if (process.env.NODE_ENV === "production") {
+      //Build PRO
       config.devtool("none");
-    } else {
+    } else if (!process.env.WEBPACK_DEV_SERVER) {
+      //BUILD DEV
       config.devtool("eval-source-map");
+    } else {
+      //LOCAL DEV
+      config.devtool("source-map");
     }
     config.plugin("define").tap(definitions => {
       definitions[0]["process.env"]["VERSION"] = JSON.stringify(
