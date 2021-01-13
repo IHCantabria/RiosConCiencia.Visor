a06d1edfdb92a5ec7eb74297c3686e304bf9aafb
David Coterillo Gonzalez <coterillogd@unican.es>
2021-01-13 16:42:58 +0100

    added devDependenci "eslint-plugin-cypress"

--- tests/e2e/specs/sanity.spec.js
+++ tests/e2e/specs/sanity.spec.js
@@ -1,6 +1,9 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("sanity test", () => {
+  before(() => {
+    cy.server();
+  });
   it("sanity test", () => {
     cy.visit("/");
   });
