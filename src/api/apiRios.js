24d2b0006d54bda80446cc6819492eda9122ec47
Sergio Cozar <97838226+SergioCozar@users.noreply.github.com>
2024-06-27 17:22:10 +0200

    updated .env config and deploy requirements

--- src/api/apiRios.js
+++ src/api/apiRios.js
@@ -1,4 +1,4 @@
-import { RIOS_API } from "@/config/data-providers.js";
+import { RIOSCONCIENCIA_API } from "@/config/data-providers.js";
 
 const _basicHeaders = {
   "Content-Type": "application/json",
@@ -7,17 +7,17 @@ const _basicHeaders = {
 };
 
 export const getAllSamples = () => {
-  const url = `${RIOS_API.public}/RiosConCiencia/GetSamplesExtended`;
+  const url = `${RIOSCONCIENCIA_API.public}/GetSamplesExtended`;
   return { url, params: _basicHeaders, method: "get", body: {} };
 };
 
 export const getAllPictsSamples = () => {
-  const url = `${RIOS_API.public}/RiosConCiencia/GetPictSamples`;
+  const url = `${RIOSCONCIENCIA_API.public}/GetPictSamples`;
   return { url, params: _basicHeaders, method: "get", body: {} };
 };
 
 export const getSamplesCsv = (token, filters) => {
-  const url = `${RIOS_API.public}/RiosConCiencia/ExportSamples/cambera`;
+  const url = `${RIOSCONCIENCIA_API.public}/ExportSamples/cambera`;
   _basicHeaders.Authorization = `Bearer ${token}`;
   const bodyObject = {
     samplesId: filters.samplesId,
@@ -27,13 +27,13 @@ export const getSamplesCsv = (token, filters) => {
 };
 
 export const getAllSamplesCsv = (token) => {
-  const url = `${RIOS_API.public}/RiosConCiencia/ExportSamplesAll/cambera`;
+  const url = `${RIOSCONCIENCIA_API.public}/ExportSamplesAll/cambera`;
   _basicHeaders.Authorization = `Bearer ${token}`;
   return { url, params: _basicHeaders, method: "getFile", body: {} };
 };
 
 export const login = (credentials) => {
-  const url = `${RIOS_API.public}/RiosConCiencia/Authenticate`;
+  const url = `${RIOSCONCIENCIA_API.public}/Authenticate`;
   const auth = {
     email: credentials.email,
     password: credentials.password,
--- src/config/data-providers.js
+++ src/config/data-providers.js
@@ -1,6 +1,5 @@
 const ENV = import.meta.env;
 
-export const RIOS_API = {
-  public: ENV.VITE_PUBLIC_RIOS,
-  private: ENV.VITE_PRIVATE_RIOS,
+export const RIOSCONCIENCIA_API = {
+  public: `${ENV.VITE_API_TOOL}/v1/public/RiosConCiencia`,
 };
