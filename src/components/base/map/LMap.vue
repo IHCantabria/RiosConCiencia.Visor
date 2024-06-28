3229cbb7ca043eaf1a6175473c0d8c6e376849f5
Sergio Cozar <97838226+SergioCozar@users.noreply.github.com>
2024-06-28 10:03:05 +0200

    fix: leaflet bugg when zooming after close popup

--- src/components/base/map/LMap.vue
+++ src/components/base/map/LMap.vue
@@ -107,7 +107,23 @@ const emit = defineEmits(["toggle-panel"]);
 // LYFE CYCLE HOOKS
 onMounted(() => {
   initMap();
+  modifyPopup();
 });
+
+const modifyPopup = () => {
+  L.Popup.prototype._animateZoom = function (e) {
+    if (!this._map) {
+      return;
+    }
+    const pos = this._map._latLngToNewLayerPoint(
+        this._latlng,
+        e.zoom,
+        e.center,
+      ),
+      anchor = this._getAnchor();
+    L.DomUtil.setPosition(this._container, pos.add(anchor));
+  };
+};
 
 // METHODS
 const initMap = async () => {
